4978a53bfb3ce5c4e859a12d1f57eec954b736f7
natsuyasai <4531735+natsuyasai@users.noreply.github.com>
2025-05-16 06:53:43 +0900

    無限ループ修正

--- webview-ui/src/hooks/useRows.ts
+++ webview-ui/src/hooks/useRows.ts
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 
 export function useRows(csvArray: Array<Array<string>>) {
+  const empty: Record<string, string>[] = [];
   const [rows, setRows] = useState((): Record<string, string>[] => createRows(csvArray));
 
   useEffect(() => {
@@ -9,16 +10,18 @@ export function useRows(csvArray: Array<Array<string>>) {
 
   function createRows(csvArray: Array<Array<string>>): Record<string, string>[] {
     if (csvArray.length === 0) {
-      return [];
+      return empty;
     }
-    return csvArray.slice(1).map((row) =>
-      row.reduce(
-        (acc, cell, colIndex) => {
-          acc[`col${colIndex}`] = cell;
-          return acc;
-        },
-        {} as Record<string, string>
-      )
+    return (
+      csvArray.slice(1).map((row) =>
+        row.reduce(
+          (acc, cell, colIndex) => {
+            acc[`col${colIndex}`] = cell;
+            return acc;
+          },
+          {} as Record<string, string>
+        )
+      ) || empty
     );
   }
 
