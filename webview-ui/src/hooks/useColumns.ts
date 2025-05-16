f910202e44bc469cfa30ff291d2bbc66a4f2f034
natsuyasai <4531735+natsuyasai@users.noreply.github.com>
2025-05-16 20:16:08 +0900

    columnとrowの生成処理をstateから計算させる形式に変更

--- webview-ui/src/hooks/useColumns.ts
+++ webview-ui/src/hooks/useColumns.ts
@@ -1,16 +1,10 @@
-import { useState, useEffect } from "react";
+import { useMemo } from "react";
 import { Column } from "react-data-grid";
 import TextAreaEditor from "@/components/TextAreaEditor";
 
 export function useColumns(csvArray: Array<Array<string>>) {
   const empty: Column<Record<string, string>>[] = [];
-  const [columns, setColumns] = useState((): Column<Record<string, string>>[] =>
-    createColumns(csvArray)
-  );
-
-  useEffect(() => {
-    setColumns(createColumns(csvArray));
-  }, [csvArray]);
+  const columns = useMemo(() => createColumns(csvArray), [csvArray]);
 
   function createColumns(csvArray: Array<Array<string>>): Column<Record<string, string>>[] {
     if (csvArray.length === 0) {
