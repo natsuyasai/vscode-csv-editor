c652bed8162263eb82e3142d83107719c11002ed
natsuyasai <yasai038tomato@gmail.com>
2025-05-17 17:17:36 +0900

    headerの定義を他に合わせる

--- webview-ui/stories/components/Header.stories.ts
+++ webview-ui/stories/components/Header.stories.ts
@@ -1,13 +1,15 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import { fn } from "@storybook/test";
 
-import Header from "@/components/Header";
+import { Header } from "@/components/Header";
 
 const meta = {
   title: "components/Header",
   component: Header,
   args: {
     isIgnoreHeaderRow: false,
+    onUpdateIgnoreHeaderRow: fn(),
+    onClickApply: fn(),
   },
 } satisfies Meta<typeof Header>;
 
@@ -17,6 +19,5 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     isIgnoreHeaderRow: false,
-    onUpdateIgnoreHeaderRow: fn(),
   },
 };
