7524bc480efdc5f89dfa5fe13cebf1e20a5ca50a
natsuyasai <4531735+natsuyasai@users.noreply.github.com>
2025-05-17 10:00:35 +0900

    storybook環境作成

---
+++ webview-ui/stories/components/Header.stories.ts
@@ -0,0 +1,22 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { fn } from "@storybook/test";
+
+import Header from "@/components/Header";
+
+const meta = {
+  title: "components/Header",
+  component: Header,
+  args: {
+    isIgnoreHeaderRow: false,
+  },
+} satisfies Meta<typeof Header>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    isIgnoreHeaderRow: false,
+    onUpdateIgnoreHeaderRow: fn(),
+  },
+};
