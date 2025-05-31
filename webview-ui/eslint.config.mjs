0699efdc87bcb265e6fd9a4a678886a786be4c3f
natsuyasai <yasai038tomato@gmail.com>
2025-05-31 10:12:29 +0900

    ui側のlinter設定

---
+++ webview-ui/eslint.config.mjs
@@ -0,0 +1,61 @@
+// @ts-check
+import pluginTypeScript from "@typescript-eslint/eslint-plugin";
+import typescriptParser from "@typescript-eslint/parser";
+import pluginPrettier from "eslint-config-prettier";
+import pluginReact from "eslint-plugin-react";
+import reactConfigRecommended from "eslint-plugin-react/configs/recommended.js";
+import pluginReactHooks from "eslint-plugin-react-hooks";
+import storybook from 'eslint-plugin-storybook'
+
+export default [
+  {
+    ignores: ["./dist"],
+  },
+  {
+    files: ["**/*.ts", "**/*.tsx"],
+    languageOptions: {
+      parser: typescriptParser,
+    },
+    plugins: {
+      "@typescript-eslint": pluginTypeScript,
+    },
+    rules: {
+      ...pluginTypeScript.configs["eslint-recommended"].rules,
+      ...pluginTypeScript.configs["recommended"].rules,
+    },
+  },
+  {
+    files: ["**/*.js", "**/*.jsx", "**/*.ts", "**/*.tsx"],
+    settings: {
+      "react": {
+        version: "detect",
+      },
+      "import/resolver": {
+        node: {
+          extensions: [".js", ".jsx", ".ts", ".tsx"],
+        },
+        typescript: {},
+      },
+    },
+    plugins: {
+      "react": pluginReact,
+      "react-hooks": pluginReactHooks,
+    },
+    rules: {
+      ...reactConfigRecommended.rules,
+      ...pluginPrettier.rules,
+      "react-hooks/rules-of-hooks": "error",
+      "react-hooks/exhaustive-deps": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          "argsIgnorePattern": "^_",
+          "varsIgnorePattern": "^_",
+          "caughtErrorsIgnorePattern": "^_",
+          "destructuredArrayIgnorePattern": "^_"
+        }
+      ],
+    },
+  },
+  ...storybook.configs['flat/recommended'],
+];
