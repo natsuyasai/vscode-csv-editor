4d01a572b8c20ce7088879cd1e7bee182e2bc1b8
natsuyasai <yasai038tomato@gmail.com>
2025-05-31 09:15:24 +0900

    ルート側のlinterの設定調整

--- .eslintrc.cjs
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  extends: [
-    "eslint:recommended",
-    "plugin:react/recommended",
-    "plugin:react/jsx-runtime",
-    "prettier",
-    "plugin:storybook/recommended"
-  ]
-}
