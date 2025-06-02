6f9e5aa69261ad1732f158ad1e068451257c6f07
natsuyasai <4531735+natsuyasai@users.noreply.github.com>
2025-06-02 03:22:36 +0000

    buildafter plugin

--- webview-ui/vite.config.ts
+++ webview-ui/vite.config.ts
@@ -2,10 +2,28 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react"
 import { resolve } from "path";
 import tailwindcss from "@tailwindcss/vite";
+import { copyFileSync } from "fs";
+
+const BuildAfterScriptPlugin = () => {
+  return {
+    name: "rollup-build-aafter-plugin",
+    closeBundle(error) {
+      try{
+        copyFileSync("src/icons/codicon.css", "build/assets/codicon.css");
+        copyFileSync("src/icons/codicon.ttf", "build/assets/codicon.ttf");
+      } catch {
+      }
+    }
+  }
+};
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    BuildAfterScriptPlugin()
+  ],
   resolve: {
     alias: {
       "@message": resolve("../src/message"),
