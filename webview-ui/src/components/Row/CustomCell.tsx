d6e191789dc55d72b3887cbcdc1a7ddb9d50d6bf
natsuyasai <yasai038tomato@gmail.com>
2025-06-05 20:34:26 +0900

    不要な処理削除

--- webview-ui/src/components/Row/CustomCell.tsx
+++ webview-ui/src/components/Row/CustomCell.tsx
@@ -1,7 +1,7 @@
-import { FC, useRef, useState } from "react";
+import { ROW_ID_KEY, ROW_IDX_KEY } from "@/types";
+import { FC, useRef } from "react";
 import { Cell, CellRendererProps } from "react-data-grid";
 import styles from "./CustomCell.module.scss";
-import { ROW_ID_KEY, ROW_IDX_KEY } from "@/types";
 
 interface Props {
   cellKey: React.Key;
@@ -14,29 +14,6 @@ export type CustomCellProps = Props & CellRendererProps<NoInfer<Record<string, s
 
 export const CustomCell: FC<CustomCellProps> = (props) => {
   const refCell = useRef<HTMLDivElement>(null);
-  const startY = useRef(0);
-  const [isResizing, setIsResizing] = useState(false);
-
-  function handleMouseDown(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    setIsResizing(true);
-    startY.current = e.clientY - (refCell.current?.getBoundingClientRect()?.height ?? 0);
-  }
-
-  function handleMouseUp(_e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    setIsResizing(false);
-  }
-
-  function handleMouseMove(e: React.MouseEvent<HTMLDivElement, MouseEvent>) {
-    if (!isResizing) {
-      return;
-    }
-    const height = e.clientY - startY.current;
-    const minHeight = 24;
-    if (height < minHeight) {
-      return;
-    }
-    props.onUpdateRowHeight(props.rowIdx, height);
-  }
 
   function handleClick() {
     if (props.column.key === ROW_IDX_KEY) {
@@ -52,9 +29,6 @@ export const CustomCell: FC<CustomCellProps> = (props) => {
       key={props.cellKey}
       className={[`${styles.cell}`, `${props.isSearchTarget ? styles.searchTarget : ""}`].join(" ")}
       {...(({ cellKey: _rowKey, onUpdateRowHeight: _onUpdateRowHeight, ...rest }) => rest)(props)}
-      onClick={handleClick}
-      onMouseDown={handleMouseDown}
-      onMouseMove={handleMouseMove}
-      onMouseUp={handleMouseUp}></Cell>
+      onClick={handleClick}></Cell>
   );
 };
