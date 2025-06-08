06d787cffad57522293d6cd2d444e440a237e1e7
natsuyasai <4531735+natsuyasai@users.noreply.github.com>
2025-06-08 10:21:13 +0900

    セル編集時にエンターで確定した場合は次の行に移動する

--- webview-ui/src/components/Row/TextAreaEditor.tsx
+++ webview-ui/src/components/Row/TextAreaEditor.tsx
@@ -1,7 +1,8 @@
-import { useEffect, useRef } from "react";
+import { useContext, useEffect, useRef } from "react";
 import { RenderEditCellProps } from "react-data-grid";
 import styles from "./TextAreaEditor.module.scss";
 import { useCellEditStore } from "@/stores/useCellEditStore";
+import { DataGridContext } from "@/contexts/dataGridContext";
 
 export default function TextAreaEditor({
   row,
@@ -11,17 +12,26 @@ export default function TextAreaEditor({
 }: RenderEditCellProps<Record<string, string>>) {
   const ref = useRef<HTMLTextAreaElement>(null);
   const initialCellKey = useCellEditStore((state) => state.initialCellKey);
+  const position = useCellEditStore((state) => state.position);
+  const clearInitialCellKey = useCellEditStore((state) => state.clearInitialCellKey);
   const clearCellEditStore = useCellEditStore((state) => state.clear);
+  const gridRef = useContext(DataGridContext);
 
   useEffect(() => {
     ref.current?.focus();
     if (ref.current && initialCellKey) {
       onRowChange({ ...row, [column.key]: initialCellKey });
     }
-    clearCellEditStore();
+    clearInitialCellKey();
     const textLen = ref.current?.value.length ?? 0;
     ref.current?.setSelectionRange(textLen, textLen);
-  }, [initialCellKey, clearCellEditStore, row, column, onRowChange]);
+  }, [initialCellKey, clearInitialCellKey, row, column, onRowChange]);
+
+  useEffect(() => {
+    return () => {
+      clearCellEditStore();
+    };
+  }, [clearCellEditStore]);
 
   return (
     <textarea
@@ -35,6 +45,13 @@ export default function TextAreaEditor({
           // DataGrid側でEnter入力時に編集モードを抜けてしまうのでイベントを止める
           e.stopPropagation();
           onRowChange({ ...row, [column.key]: (e.target as HTMLTextAreaElement).value });
+        } else if (e.key === "Enter" && position) {
+          // 行のサイズを超えたら移動は起こらないため、チェックせずに+1
+          const nextRow = position.rowIdx + 1;
+          gridRef?.selectCell({
+            idx: position.idx,
+            rowIdx: nextRow,
+          });
         }
       }}
     />
