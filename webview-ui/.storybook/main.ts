0e58d0869cc0ffb6b3a906e40f1f330401252021
natsuyasai <4531735+natsuyasai@users.noreply.github.com>
2025-06-15 21:08:38 +0900

    一部アドオン無効化

--- webview-ui/.storybook/main.ts
+++ webview-ui/.storybook/main.ts
@@ -7,7 +7,7 @@ const config: StorybookConfig = {
     "@storybook/addon-essentials",
     "@storybook/addon-onboarding",
     "@chromatic-com/storybook",
-    "@storybook/experimental-addon-test",
+    //"@storybook/experimental-addon-test",
   ],
   framework: {
     name: "@storybook/react-vite",
