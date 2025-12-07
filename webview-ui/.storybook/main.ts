a694deab3941c99d5492ece8824de0c0fff8949f
natsuyasai <4531735+natsuyasai@users.noreply.github.com>
2025-12-07 18:10:29 +0900

    __dirnameが見つからない問題を修正

--- webview-ui/.storybook/main.ts
+++ webview-ui/.storybook/main.ts
@@ -1,5 +1,10 @@
 import path from "path";
+import { fileURLToPath } from "url";
 import type { StorybookConfig } from "@storybook/react-vite";
+
+const __filename = fileURLToPath(import.meta.url);
+
+const __dirname = path.dirname(__filename);
 
 const config: StorybookConfig = {
   stories: ["../stories/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
